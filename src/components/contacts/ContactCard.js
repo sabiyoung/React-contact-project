27bbefe449b2f59b67ec70b602171499d3123af6
sabiyoung <ethiodatecakes@gmail.com>
2021-06-03 08:09:02 -0700

    updated the avatar

--- src/components/contacts/ContactCard.js
+++ src/components/contacts/ContactCard.js
@@ -1,69 +1,81 @@
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 
-import styled from 'styled-components';
-import {Avatar,  } from "@material-ui/core";
-import CloseIcon from '@material-ui/icons/Close';
-
-const ContactCard = ({ firstName='', lastName='', phoneNumber='', profilePic='' }) => {
-    return (
-        <CardContainer className="wrapper">
-            <CardContent className="cardContent">
-                <ImgAvatar className="avatar">
-                    <img src={profilePic} alt={`Profile of ${firstName} ${lastName}.`} />
-                </ImgAvatar>
-                <ContactInfo>
-                    <p>{firstName} {lastName}</p>
-                    <p>{phoneNumber}</p>
-                 
-                </ContactInfo>
-                <NewButton >
-                {/* <button className="button">X</button> */}
-                <CloseIcon className="button"/>
-            </NewButton>
-            </CardContent>
+const ContactCard = ({
+  firstName = "",
+  lastName = "",
+  phoneNumber = "",
+  profilePic = "",
+}) => {
+  return (
+    <CardContainer className="wrapper">
+      <CardContent className="cardContent">
+      <div className="avatar"> 
+        <ImgAvatar >
+          <img src={profilePic} alt={`Profile of ${firstName} ${lastName}.`} />
+        </ImgAvatar>
+        </div>
+        <ContactInfo>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <p>{phoneNumber}</p>
+        </ContactInfo>
+        <NewButton>
           
-        </CardContainer>
-    )
-}
+          <CloseIcon className="button" />
+        </NewButton>
+      </CardContent>
+    </CardContainer>
+  );
+};
 
 const CardContainer = styled.div`
-
- z-index: 1;
+  z-index: 1;
   background-color: white;
   height: 130px;
   width: 800px;
   box-shadow: 0 6px 18px -9px rgba(0, 0, 0, 0.75);
   margin-top: 30px;
- margin-left:80px;
- 
-
+  margin-left: 80px;
 `;
 
 const CardContent = styled.div`
-display: flex;
-justify-content: space-between;
-padding: 40px;
+  display: flex;
+  justify-content: space-between;
+  padding: 40px;
 
-img {
-    width: 100%; 
-    
-}`;
+  img {
+    width: 100%;
+  }
+  .avatar {
+    border-right: 1px solid lightgray;
+    padding-right:20px;
+  
+}
+`;
 const ImgAvatar = styled(Avatar)`
-    height: 200px;
-    border-left: 1px solid rgb(82, 75, 75);
+min-width:100px;
+min-height:100px;
+margin-top:-20px;
+ cursor:pointer;
+
+:hover {
+    opacity: 0.8;
+}
 
 `;
 
 const ContactInfo = styled.div`
-
 `;
 const NewButton = styled.div`
-.button{
+  .button {
     color: white;
     background-color: lightgray;
-    border-radius:50%;
+    border-radius: 50%;
     border: lightgray;
     padding: 2px;
-}
-
+  }
 `;
 export default ContactCard;
--- src/components/contacts/ContactsList.js
+++ src/components/contacts/ContactsList.js
@@ -1,57 +1,51 @@
-import ContactCard from "./ContactCard"
+import ContactCard from "./ContactCard";
 import styled from "styled-components";
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 
-const ContactList = ({ contacts=[] }) => {
-    return (
-   <MainContainer> 
-     <Title className="title">
-          <RadioButtonUncheckedIcon className="radio"/> 
-       <p>ContactList</p>
-       </Title>
-      
-        <ContactListContainer className="contactListContainer">
-        
-            {contacts.map((contact) => 
-                <ContactCard 
-                    key={contact.id}
-                    firstName={contact.firstName} 
-                    lastName={contact.lastName} 
-                    phoneNumber={contact.phoneNumber} 
-                    profilePic={contact.profilePic}
-                />
-            )}
-        </ContactListContainer>
-        </MainContainer>
-    )
-}
+const ContactList = ({ contacts = [] }) => {
+  return (
+    <MainContainer>
+      <Title className="title">
+        <RadioButtonUncheckedIcon className="radio" />
+        <p>ContactList</p>
+      </Title>
+
+      <ContactListContainer className="contactListContainer">
+        {contacts.map((contact) => (
+          <ContactCard
+            key={contact.id}
+            firstName={contact.firstName}
+            lastName={contact.lastName}
+            phoneNumber={contact.phoneNumber}
+            profilePic={contact.profilePic}
+          />
+        ))}
+      </ContactListContainer>
+    </MainContainer>
+  );
+};
 
 export default ContactList;
 
 const ContactListContainer = styled.div`
-background-color: lightgray;
-border: solid 1px gray;
-height: 670px;
-width: 50%;
+  background-color: lightgray;
+  border: solid 1px gray;
+  height: 670px;
+  width: 50%;
 
-margin-left:500px;
-padding:40px 0 40px 0;
-
-
+  margin-left: 500px;
+  padding: 40px 0 40px 0;
 `;
 const Title = styled.div`
-display: flex;
-align-items: center;
-/* margin-top: 100px; */
-margin-left:500px;
-p {
+  display: flex;
+  align-items: center;
+  margin-left: 500px;
+  p {
     font-size: 35px;
-    padding-left:7px;
-}
-.radio {
+    padding-left: 7px;
+  }
+  .radio {
     height: 18px;
-}
+  }
 `;
-const MainContainer = styled.div`
-
-`;
+const MainContainer = styled.div``;
